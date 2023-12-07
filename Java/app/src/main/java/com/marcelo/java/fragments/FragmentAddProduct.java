dcb7408fb1ee086c050831718aebbac46407cbb3
Marcelo <marcelox.ofl@gmail.com>
2023-12-07 16:17:27 -0300

    Fragments

---
+++ Java/app/src/main/java/com/marcelo/java/fragments/FragmentAddProduct.java
@@ -0,0 +1,23 @@
+package com.marcelo.java.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.marcelo.java.R;
+
+public class FragmentAddProduct extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_product, container, false);
+
+
+        return view;
+    }
+}
---
+++ Java/app/src/main/java/com/marcelo/java/fragments/FragmentManagerProduct.java
@@ -0,0 +1,21 @@
+package com.marcelo.java.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.marcelo.java.R;
+
+public class FragmentManagerProduct extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_manager_product, container, false);
+        return view;
+    }
+}
